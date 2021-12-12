e8204184c0251944614b9d5cda5ee9c9533dca75
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-12-12 15:42:55 +0300

    inserts navbar

--- awardee/urls.py
+++ awardee/urls.py
@@ -1,2 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
+from django.urls import path
+urlpatterns=[
+    path('', views.home, name='home'),
+]
--- awardee/views.py
+++ awardee/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render,redirect
+from django.contrib.auth.decorators import login_required
+from django.http  import HttpResponse,Http404
+from .models import Project,Profile,Rating
 
 # Create your views here.
+def home(request):
+    return render(request,'index.html')
+

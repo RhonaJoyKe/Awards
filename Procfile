cfe867fa4176d084abe6770c31e48e7021a0267b
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-12-14 20:20:34 +0300

    configures django heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn awarrds.wsgi --log-file -
--- awards/settings.py
+++ awards/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,3 +145,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
